b3f7a63a8f49ad4721cda76892f0d0baa2e78771
Jesus Navarro romero <jenavarr@car6s4.42barcelona.com>
2023-04-10 21:23:24 +0200

    ñiñiñ

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: jenavarr <jenavarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 17:27:04 by jenavarr          #+#    #+#             */
-/*   Updated: 2023/04/10 17:36:46 by jenavarr         ###   ########.fr       */
+/*   Updated: 2023/04/10 21:15:25 by jenavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,9 @@ void	init_system(t_system *sys)
 	fd = open(sys->filename, O_RDONLY);
 	load_map(sys, fd);
 	close(fd);
-	// put_pixel(sys->img, 10, 10, 0xFFFFFF);
-	// draw_line(sys, (int []){0, 0}, (int []){1920, 1080}, 0xFFFFFF);
 	ft_printf("Number of points is: %i\n", sys->map.length);
 	ft_printf("Number of points per line is: %i\n", sys->map.ll);
 	render(sys);
-	// debug_points(sys);
 	debug_line(sys);
 	mlx_hook(sys->mlx_win, 17, 0, f_exit, "");
 	mlx_loop(sys->mlx_ptr);
